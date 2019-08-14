5779b8304e3e2258ec6b0291130f7be47550125d
Cara Yung <yungcara@gmail.com>
2019-08-13 20:58:25 -0400

    Add command to kick a user

--- index.js
+++ index.js
@@ -16,4 +16,28 @@ client.on('guildMemberAdd', member => {
     member.send(`Welcome to the server ${member}!`);
   });
 
+client.on('message', async message => {
+    if (message.content.startsWith('!kick')) {
+        const member = message.mentions.members.first()
+
+        if (!member) {
+        return message.reply(
+            `Who are you trying to kick? You must mention a user.`
+        )
+        }
+
+        if (!member.kickable) {
+        return message.reply(`I can't kick this user. Sorry!`)
+        }
+
+        try {
+            await member.kick();
+            return await message.reply(`${member.user.tag} was kicked.`);
+        }
+        catch (error) {
+            return await message.reply(`Sorry, an error occured.`);
+        }
+        }
+});
+
 client.login(process.env.BOT_TOKEN)

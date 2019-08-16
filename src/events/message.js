d90b37f01392b574e7a7492e212106a3a6515b44
Cara Yung <yungcara@gmail.com>
2019-08-15 21:33:24 -0400

    Add filter file

--- src/events/message.js
+++ src/events/message.js
@@ -1,4 +1,5 @@
 const kick = require('../commands/kick')
+var { filter } = require('../filter')
 
 module.exports = (client, message) => {
     if (message.content === '!hello') {
@@ -6,5 +7,12 @@ module.exports = (client, message) => {
     }
     else if (message.content.startsWith('!kick')) {
         return kick(message)
+    }
+    else if (message.content.startsWith('!bannedWords')) {
+        if (filter.list.length == 0) {
+            message.reply('Right now there are no banned words!')
+        } else {
+            message.author.send(`Here's the current list of banned words: ${filter.list}`)
+        }
     }
 }
---
+++ src/filter.js
@@ -0,0 +1,4 @@
+var Filter = require('bad-words'),
+
+filter = new Filter({ emptyList: true })
+module.exports = { filter };

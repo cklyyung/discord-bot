3d086f1539fa9902b3d84ac54962ca1ed6a0a2b6
Cara Yung <yungcara@gmail.com>
2019-08-20 20:36:21 -0400

    Track how many times a user has said a banned word

--- src/commands/censor.js
+++ src/commands/censor.js
@@ -1,8 +1,21 @@
+const redis = require('../redis.js')
+
 module.exports = (message, bannedWord) => {
     message.delete()
     .then(msg => console.log(`Deleted message with banned word ${bannedWord} by ${msg.author.username}`))
     .catch(console.error);
 
-    message.reply(`Your message has been removed; ${bannedWord} is a banned word! Please use the command !bannedWords to check which words are banned.`)
+    var warningMessage = `Your message has been removed; ${bannedWord} is a banned word!`
+    const user = message.author.toString()
 
+    redis.getKey(user, function(strikes) {
+        console.log(strikes)
+        if (strikes == -1) {
+            strikes = 0
+        }
+        strikes++
+        warningMessage = warningMessage.concat(` This is strike #${strikes}.`)
+        redis.storeKey(user, strikes)
+        message.reply(warningMessage)
+    })
 }
